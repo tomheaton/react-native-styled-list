35592c219b9c650a6fa917036181994240cbccf2
Thomas Heaton <thomas.m.heaton@gmail.com>
2022-08-16 16:03:58 +0100

    rework

--- src/StyledList.tsx
+++ src/StyledList.tsx
@@ -1,63 +1,6 @@
 import React, {Children, ReactNode} from "react";
 import {View, Text, StyleSheet} from "react-native";
 
-type Props = {
-    children: ReactNode
-    containerStyle?: any
-    childStyle?: any
-}
-
-const StyledList: React.FC<Props> = ({children, containerStyle, childStyle}) => {
-    const childrenArray = Children.toArray(children);
-
-    return (
-        <View style={[styles.container, containerStyle]}>
-            {childrenArray.map((child, index) => {
-                return (
-                    // TODO: render bullet point in a row view to keep left padding on new lines
-                    <Text key={index} style={[styles.child, childStyle]}>
-                        {"\u2022"} {child}
-                    </Text>
-                );
-            })}
-        </View>
-    );
-}
-
-export default StyledList;
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'white',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    child: {
-        color: 'black',
-    },
-    row: {
-        flexDirection: "row",
-        // TODO: check this value
-        marginVertical: 2,
-    },
-    customBulletContainer: {
-        // TODO: check this value
-        marginRight: 4,
-    },
-    bulletContainer: {
-        // TODO: check these values
-        fontSize: 16,
-        marginRight: 4,
-    },
-    numberContainer: {
-        fontWeight: "bold",
-        // TODO: check these values
-        fontSize: 16,
-        marginRight: 4,
-    }
-});
-
 const bulletStyles = {
     regular: "\u2022",
     large: "\u25CF",
@@ -128,3 +71,35 @@ export const NumberedList: React.FC<NumberedProps> = ({children, containerStyle,
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    child: {
+        color: 'black',
+    },
+    row: {
+        flexDirection: "row",
+        // TODO: check this value
+        marginVertical: 2,
+    },
+    customBulletContainer: {
+        // TODO: check this value
+        marginRight: 4,
+    },
+    bulletContainer: {
+        // TODO: check these values
+        fontSize: 16,
+        marginRight: 4,
+    },
+    numberContainer: {
+        fontWeight: "bold",
+        // TODO: check these values
+        fontSize: 16,
+        marginRight: 4,
+    }
+});
